126e909f824d7c869ab7a68d89963e69d54d3c41
Mahesh4229 <156931842+Mahesh4229@users.noreply.github.com>
2025-04-15 21:46:22 -0400

    Update batting_vs_chasing.js
    mg3

--- batting_vs_chasing.js
+++ batting_vs_chasing.js
@@ -29,20 +29,63 @@ d3.csv("Cleaned_Cricket_Match_Dataset@1.csv").then(data => {
   const x = d3.scaleBand().domain(dataset.map(d => d.team)).range([0, width]).padding(0.2);
   const y = d3.scaleLinear().domain([0, d3.max(dataset, d => Math.max(d.battingFirst, d.chasing))]).nice().range([height, 0]);
 
+  const tooltip = d3.select("body").append("div")
+    .attr("class", "tooltip")
+    .style("opacity", 0);
+
   svg.selectAll(".batting").data(dataset).enter().append("rect")
     .attr("x", d => x(d.team))
     .attr("y", d => y(d.battingFirst))
     .attr("width", x.bandwidth() / 2)
     .attr("height", d => height - y(d.battingFirst))
-    .attr("fill", "orange");
+    .attr("fill", "orange")
+    .on("mouseover", function(event, d) {
+      tooltip.transition().duration(200).style("opacity", .9);
+      tooltip.html(`${d.team} Batting First Wins: ${d.battingFirst}`)
+        .style("left", `${event.pageX + 5}px`)
+        .style("top", `${event.pageY - 28}px`);
+    })
+    .on("mouseout", function(d) {
+      tooltip.transition().duration(500).style("opacity", 0);
+    });
 
   svg.selectAll(".chasing").data(dataset).enter().append("rect")
     .attr("x", d => x(d.team) + x.bandwidth() / 2)
     .attr("y", d => y(d.chasing))
     .attr("width", x.bandwidth() / 2)
     .attr("height", d => height - y(d.chasing))
-    .attr("fill", "green");
+    .attr("fill", "green")
+    .on("mouseover", function(event, d) {
+      tooltip.transition().duration(200).style("opacity", .9);
+      tooltip.html(`${d.team} Chasing Wins: ${d.chasing}`)
+        .style("left", `${event.pageX + 5}px`)
+        .style("top", `${event.pageY - 28}px`);
+    })
+    .on("mouseout", function(d) {
+      tooltip.transition().duration(500).style("opacity", 0);
+    });
 
   svg.append("g").attr("transform", `translate(0,${height})`).call(d3.axisBottom(x)).selectAll("text").attr("transform", "rotate(-45)").style("text-anchor", "end");
   svg.append("g").call(d3.axisLeft(y));
+
+  // Add labels
+  svg.selectAll("text.label")
+    .data(dataset)
+    .enter().append("text")
+    .attr("x", d => x(d.team) + x.bandwidth() / 4)
+    .attr("y", d => y(d.battingFirst) - 5)
+    .attr("text-anchor", "middle")
+    .text(d => d.battingFirst)
+    .style("fill", "#fff")
+    .style("font-size", "10px");
+
+  svg.selectAll("text.chasingLabel")
+    .data(dataset)
+    .enter().append("text")
+    .attr("x", d => x(d.team) + x.bandwidth() * 3 / 4)
+    .attr("y", d => y(d.chasing) - 5)
+    .attr("text-anchor", "middle")
+    .text(d => d.chasing)
+    .style("fill", "#fff")
+    .style("font-size", "10px");
 });

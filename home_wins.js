979ddc38a369d7854f9fc7fe552938ecae59a0c3
Mahesh4229 <156931842+Mahesh4229@users.noreply.github.com>
2025-04-15 21:45:55 -0400

    Update home_wins.js
    mg2

--- home_wins.js
+++ home_wins.js
@@ -17,6 +17,10 @@ d3.csv("Cleaned_Cricket_Match_Dataset@1.csv").then(data => {
   const x = d3.scaleBand().domain(dataset.map(d => d.team)).range([0, width]).padding(0.1);
   const y = d3.scaleLinear().domain([0, d3.max(dataset, d => d.wins)]).nice().range([height, 0]);
 
+  const tooltip = d3.select("body").append("div")
+    .attr("class", "tooltip")
+    .style("opacity", 0);
+
   svg.append("g")
     .selectAll("rect")
     .data(dataset)
@@ -25,7 +29,16 @@ d3.csv("Cleaned_Cricket_Match_Dataset@1.csv").then(data => {
     .attr("y", d => y(d.wins))
     .attr("width", x.bandwidth())
     .attr("height", d => height - y(d.wins))
-    .attr("fill", "steelblue");
+    .attr("fill", "steelblue")
+    .on("mouseover", function(event, d) {
+      tooltip.transition().duration(200).style("opacity", .9);
+      tooltip.html(`${d.team} Wins: ${d.wins}`)
+        .style("left", `${event.pageX + 5}px`)
+        .style("top", `${event.pageY - 28}px`);
+    })
+    .on("mouseout", function(d) {
+      tooltip.transition().duration(500).style("opacity", 0);
+    });
 
   svg.append("g")
     .attr("transform", `translate(0,${height})`)
@@ -35,4 +48,15 @@ d3.csv("Cleaned_Cricket_Match_Dataset@1.csv").then(data => {
     .style("text-anchor", "end");
 
   svg.append("g").call(d3.axisLeft(y));
+
+  // Add labels
+  svg.selectAll("text.label")
+    .data(dataset)
+    .enter().append("text")
+    .attr("x", d => x(d.team) + x.bandwidth() / 2)
+    .attr("y", d => y(d.wins) - 5)
+    .attr("text-anchor", "middle")
+    .text(d => d.wins)
+    .style("fill", "#fff")
+    .style("font-size", "12px");
 });
